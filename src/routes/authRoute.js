87f55aaa1eea0cfab7700b9153f3efc907789947
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-12 00:16:05 +0530

    jwt created

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 router.post("/signup", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.create({ email, password });
-    res.send("Post route");
+    const token = jwt.sign({ userId: user._id }, process.env.jwt_key);
+    res.send({ token });
   } catch (err) {
     return res.status(422).send(err.message);
   }
