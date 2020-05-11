b4f4a327974f98dc02310bfb7a5e69c6580f4c87
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-12 00:10:01 +0530

    error handling

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -4,8 +4,12 @@ const User = mongoose.model("User");
 
 const router = express.Router();
 router.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.create({ email, password });
-  res.send("Post route");
+  try {
+    const { email, password } = req.body;
+    const user = await User.create({ email, password });
+    res.send("Post route");
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 module.exports = router;
