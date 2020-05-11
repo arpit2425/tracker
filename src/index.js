f4b371fe48745759dc032756d8dacfe723cd3a2a
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-11 23:37:58 +0530

    server setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-  res.send('HI')
-})
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: `${__dirname}/../config.env` });
+const app = express();
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("HI");
+});
 app.listen(3000, () => {
-  console.log('listening')
-})
+  console.log("listening");
+});
