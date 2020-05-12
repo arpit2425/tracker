c4af0080fac08dbb07fde6fa8e048f9af3ca7608
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-13 01:29:24 +0530

    Signin route

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -10,5 +11,34 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      this.password = hash;
+      next();
+    });
+  });
+});
+userSchema.methods.comparePasswords = function (pass) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(pass, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      } else if (!isMatch) {
+        return reject(false);
+      }
+      resolve(true);
+    });
+  });
+};
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -14,4 +14,22 @@ router.post("/signup", async (req, res) => {
     return res.status(422).send(err.message);
   }
 });
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ error: "please provide email and password" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).send({ error: "Incorrect email or password" });
+  }
+  try {
+    await user.comparePasswords(password);
+    const token = jwt.sign({ userId: user._id }, process.env.jwt_key);
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Incorrect email or password" });
+  }
+});
 module.exports = router;
