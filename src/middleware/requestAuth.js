b1a8bab63b5431e4ad8f5c561a64a787d8ff2d20
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-12 23:58:45 +0530

    updated Auth Middleware

--- src/middleware/requestAuth.js
+++ src/middleware/requestAuth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const express = require("express");
+const User = mongoose.model("User");
 module.exports = (req, res, next) => {
   const { authoraization } = req.headers;
   if (!authoraization) {
@@ -11,6 +12,8 @@ module.exports = (req, res, next) => {
     if (err) {
       return res.status(401).send({ message: "You must  login " });
     }
+    const { userId } = payload;
+    const user = await User.findById(userId);
   });
   next();
 };
