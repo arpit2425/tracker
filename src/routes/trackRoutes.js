9a24a1f155fb015d6d83bd7191325d45b2ebd0b1
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-13 18:19:35 +0530

    Track creation route

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -8,4 +8,18 @@ router.get("/tracks", async (req, res) => {
   const tracks = await Track.find({ userId: req.user._id });
   res.send({ tracks });
 });
+router.post("/tracks", async (req, res) => {
+  const { name, locations } = req.body;
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ message: "Please provide name and location of track" });
+  }
+  try {
+    const track = await Track.create({ name, locations, userId: req.user._id });
+    res.send({ track });
+  } catch (err) {
+    return res.status(422).send({ error: err.message });
+  }
+});
 module.exports = router;
